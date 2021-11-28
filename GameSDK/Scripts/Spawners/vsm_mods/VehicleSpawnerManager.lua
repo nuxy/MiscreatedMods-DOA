9741b186842aae68f6441dfffb3d3b26d02d5ee7
Marc S. Brooks <marc@mbrooks.info>
2021-11-28 09:25:32 -0800

    Relocated to vsm_mods directory (bugfix)
    Updated initialMinVehicles spawn limits

--- GameSDK/Scripts/Spawners/vsm_mods/VehicleSpawnerManager.lua
+++ GameSDK/Scripts/Spawners/vsm_mods/VehicleSpawnerManager.lua
@@ -1,57 +1,76 @@
 -- Override defaults.
 local vehicle1 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "armored_truck_army")
 vehicle1.abandonedTimer = 2630000 -- one month
+vehicle1.initialMinVehicles = 2
 
 local vehicle2 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "armored_truck_police")
 vehicle2.abandonedTimer = 2630000 -- one month
+vehicle2.initialMinVehicles = 2
 
 local vehicle3 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "bicycle")
 vehicle3.abandonedTimer = 2630000 -- one month
+vehicle3.initialMinVehicles = 10
 
 local vehicle4 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "dirtbike")
 vehicle4.abandonedTimer = 2630000 -- one month
+vehicle4.initialMinVehicles = 7
 
 local vehicle5 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "dune_buggy")
 vehicle5.abandonedTimer = 2630000 -- one month
+vehicle5.initialMinVehicles = 3
 
 local vehicle6 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "f100truck")
 vehicle6.abandonedTimer = 2630000 -- one month
+vehicle6.initialMinVehicles = 2
 
 local vehicle7 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "fishing_boat")
 vehicle7.abandonedTimer = 2630000 -- one month
+vehicle7.initialMinVehicles = 3
 
 local vehicle8 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "jetski")
 vehicle8.abandonedTimer = 2630000 -- one month
+vehicle8.initialMinVehicles = 5
 
 local vehicle9 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "party_bus")
 vehicle9.abandonedTimer = 2630000 -- one month
+vehicle9.initialMinVehicles = 1
 
 local vehicle10 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "quadbike")
 vehicle10.abandonedTimer = 2630000 -- one month
+vehicle10.initialMinVehicles = 5
 
 local vehicle11 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "sedan_base")
 vehicle11.abandonedTimer = 2630000 -- one month
+vehicle11.initialMinVehicles = 2
 
 local vehicle12 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "sedan_police")
 vehicle12.abandonedTimer = 2630000 -- one month
+vehicle12.initialMinVehicles = 4
 
 local vehicle13 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "sedan_taxi")
 vehicle13.abandonedTimer = 2630000 -- one month
+vehicle13.initialMinVehicles = 4
 
 local vehicle14 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "suv_basic")
 vehicle14.abandonedTimer = 2630000 -- one month
+vehicle14.initialMinVehicles = 2
 
 local vehicle15 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "suv_police")
 vehicle15.abandonedTimer = 2630000 -- one month
+vehicle15.initialMinVehicles = 2
 
 local vehicle16 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "towcar")
 vehicle16.abandonedTimer = 2630000 -- one month
+vehicle16.initialMinVehicles = 2
 
 local vehicle17 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "tractor")
 vehicle17.abandonedTimer = 2630000 -- one month
+vehicle17.initialMinVehicles = 2
 
 local vehicle18 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "truck_5ton")
 vehicle18.abandonedTimer = 2630000 -- one month
+vehicle18.initialMinVehicles = 2
 
 local vehicle19 = FindInTable(VehicleSpawnerManager.vehicleCategories, "category", "truck_semi")
 vehicle19.abandonedTimer = 2630000 -- one month
+vehicle19.initialMinVehicles = 2
