27738c5237c7ef805aa42ce7431d740401327b9f
Marc S. Brooks <marc@mbrooks.info>
2021-11-28 10:08:09 -0800

    Human spiders - I gotta have more spiders!
    Updated existing mutant spawn totals

--- GameSDK/Scripts/Spawners/ism_mods/AISpawnerManager.lua
+++ GameSDK/Scripts/Spawners/ism_mods/AISpawnerManager.lua
@@ -22,3 +22,46 @@ RemoveFromTable(AISpawnerManager.aiCategories, bug5)
 local bug6 = FindInTable(AISpawnerManager.aiCategories, "category", "ai_actionable_critter")
 
 RemoveFromTable(AISpawnerManager.aiCategories, bug6)
+
+-- Add more human spiders.
+local mutant1 = FindInTable(AISpawnerManager.aiCategories, "category", "Spiker")
+mutant1.classes =
+{
+	{ category = "CrazySingle", percent = 30 },
+	{ category = "SpikerSingle", percent = 20 },
+	{ category = "HumanSpiderSingle", percent = 20 },
+	{ category = "HumanZombieSingle", percent = 15 },
+	{ category = "BruteMutantSingle", percent = 10 },
+	{ category = "spiker_pack", percent = 5 },
+}
+
+local mutant2 = FindInTable(AISpawnerManager.aiCategories, "category", "spiker_pack")
+mutant2.min = 4
+mutant2.max = 5
+
+local mutant3 = FindInTable(AISpawnerManager.aiCategories, "category", "brute_pack")
+mutant3.min = 4
+mutant3.max = 6
+
+local mutant4 = FindInTable(AISpawnerManager.aiCategories, "category", "brute_hunting_pack")
+mutant4.min = 4
+mutant4.max = 7
+
+local mutant5 = FindInTable(AISpawnerManager.aiCategories, "category", "horde")
+mutant5.classes =
+{
+	{ category = "CrazySingle", percent = 30 },
+	{ category = "SpikerSingle", percent = 20 },
+	{ category = "HumanSpiderSingle", percent = 20 },
+	{ category = "HumanZombieSingle", percent = 20 },
+	{ category = "BruteMutantSingle", percent = 10 },
+}
+
+local mutant6 = FindInTable(AISpawnerManager.aiCategories, "category", "mutant_invasion")
+mutant6.classes =
+{
+	{ category = "CrazySingle", percent = 30 },
+	{ category = "SpikerSingle", percent = 20 },
+	{ category = "HumanSpiderSingle", percent = 30 },
+	{ category = "HumanZombieSingle", percent = 20 },
+}
