5b6abd9785109e18da6cb3ef58c7c096ba3fca69
Marc S. Brooks <marc@mbrooks.info>
2021-12-02 11:39:46 -0800

    Replaced all mutants with Human Spiders

--- GameSDK/Scripts/Spawners/aism_mods/MoreHumanSpiders.lua
+++ GameSDK/Scripts/Spawners/aism_mods/MoreHumanSpiders.lua
@@ -1,41 +1,52 @@
 local mutant1 = FindInTable(AISpawnerManager.aiCategories, "category", "Spiker")
 mutant1.classes =
 {
-	{ category = "CrazySingle", percent = 30 },
-	{ category = "SpikerSingle", percent = 20 },
+	{ category = "HumanSpiderSingle", percent = 30 },
+	{ category = "HumanSpiderSingle", percent = 20 },
 	{ category = "HumanSpiderSingle", percent = 20 },
 	{ category = "HumanZombieSingle", percent = 15 },
-	{ category = "BruteMutantSingle", percent = 10 },
-	{ category = "spiker_pack", percent = 5 },
+	{ category = "HumanSpiderSingle", percent = 10 },
+	{ category = "HumanSpiderSingle", percent = 5 },
 }
 
-local mutant2 = FindInTable(AISpawnerManager.aiCategories, "category", "spiker_pack")
-mutant2.min = 4
-mutant2.max = 5
-
-local mutant3 = FindInTable(AISpawnerManager.aiCategories, "category", "brute_pack")
-mutant3.min = 4
-mutant3.max = 6
-
-local mutant4 = FindInTable(AISpawnerManager.aiCategories, "category", "brute_hunting_pack")
-mutant4.min = 4
-mutant4.max = 7
-
-local mutant5 = FindInTable(AISpawnerManager.aiCategories, "category", "horde")
-mutant5.classes =
+local mutant2 = FindInTable(AISpawnerManager.aiCategories, "category", "Brute")
+mutant2.classes =
 {
-	{ category = "CrazySingle", percent = 30 },
-	{ category = "SpikerSingle", percent = 20 },
+	{ category = "HumanSpiderSingle", percent = 30 },
 	{ category = "HumanSpiderSingle", percent = 20 },
-	{ category = "HumanZombieSingle", percent = 20 },
-	{ category = "BruteMutantSingle", percent = 10 },
+	{ category = "HumanSpiderSingle", percent = 20 },
+	{ category = "HumanZombieSingle", percent = 15 },
+	{ category = "HumanSpiderSingle", percent = 10 },
+	{ category = "HumanSpiderSingle", percent = 5 },
 }
 
-local mutant6 = FindInTable(AISpawnerManager.aiCategories, "category", "mutant_invasion")
+local mutant3 = FindInTable(AISpawnerManager.aiCategories, "category", "spiker_pack")
+mutant3.min = 4
+mutant3.max = 5
+
+local mutant4 = FindInTable(AISpawnerManager.aiCategories, "category", "brute_pack")
+mutant4.min = 4
+mutant4.max = 6
+
+local mutant5 = FindInTable(AISpawnerManager.aiCategories, "category", "brute_hunting_pack")
+mutant5.min = 4
+mutant5.max = 7
+
+local mutant6 = FindInTable(AISpawnerManager.aiCategories, "category", "horde")
 mutant6.classes =
 {
-	{ category = "CrazySingle", percent = 30 },
-	{ category = "SpikerSingle", percent = 20 },
 	{ category = "HumanSpiderSingle", percent = 30 },
-	{ category = "HumanZombieSingle", percent = 20 },
+	{ category = "HumanSpiderSingle", percent = 20 },
+	{ category = "HumanSpiderSingle", percent = 20 },
+	{ category = "HumanSpiderSingle", percent = 20 },
+	{ category = "HumanSpiderSingle", percent = 10 },
+}
+
+local mutant7 = FindInTable(AISpawnerManager.aiCategories, "category", "mutant_invasion")
+mutant7.classes =
+{
+	{ category = "HumanSpiderSingle", percent = 30 },
+	{ category = "HumanSpiderSingle", percent = 20 },
+	{ category = "HumanSpiderSingle", percent = 30 },
+	{ category = "HumanSpiderSingle", percent = 20 },
 }
---
+++ GameSDK/Scripts/Spawners/rrai_mods/MoreHumanSpiders.lua
@@ -0,0 +1,44 @@
+local mutant1 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIGlobal_Spider")
+mutant1.spawnCategory = "HumanSpiderSingle"
+
+local mutant2 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIGlobal_Roach")
+mutant2.spawnCategory = "HumanSpiderSingle"
+
+local mutant3 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_HumanZombie")
+mutant3.spawnCategory = "HumanSpiderSingle"
+
+local mutant4 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_TwoHeadDog")
+mutant4.spawnCategory = "HumanSpiderSingle"
+
+local mutant5 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_Brute")
+mutant5.spawnCategory = "HumanSpiderSingle"
+
+local mutant6 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_BruteArmored")
+mutant6.spawnCategory = "HumanSpiderSingle"
+
+local mutant7 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_Spider")
+mutant7.spawnCategory = "HumanSpiderSingle"
+
+local mutant8 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_Roach")
+mutant8.spawnCategory = "HumanSpiderSingle"
+
+local mutant9 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_Crazy")
+mutant9.spawnCategory = "HumanSpiderSingle"
+
+local mutant10 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_Spiker")
+mutant10.spawnCategory = "HumanSpiderSingle"
+
+local mutant11 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_HumanZombie")
+mutant11.spawnCategory = "HumanSpiderSingle"
+
+local mutant12 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_TwoHeadDog")
+mutant12.spawnCategory = "HumanSpiderSingle"
+
+local mutant13 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_Brute")
+mutant13.spawnCategory = "HumanSpiderSingle"
+
+local mutant14 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_Crazy")
+mutant14.spawnCategory = "HumanSpiderSingle"
+
+local mutant15 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_Spiker")
+mutant15.spawnCategory = "HumanSpiderSingle"
