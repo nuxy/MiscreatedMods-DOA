1a6ede135de37ce04e27067eec4ae8a8184dcb5d
Marc S. Brooks <marc@mbrooks.info>
2021-12-02 10:39:47 -0800

    Equip new/respawned players (custom items)

--- GameSDK/Scripts/GameRules/Miscreated.lua
+++ GameSDK/Scripts/GameRules/Miscreated.lua
@@ -120,26 +120,103 @@ function Miscreated:RevivePlayer(playerId)
   end
 --]]
 
--- If this method is defined, then Miscreated will ONLY spawn items for a new or respawned
--- player based on the code below.
--- This is called after the player starts the spawning process
--- Add any custom equipment or other finalizing touches here
---[[
 function Miscreated:EquipPlayer(playerId)
-	--Log(">> Miscreated:EquipPlayer");
+	--Log(">> Miscreated:EquipPlayer")
 	
-	-- Get the entity for the player
-	local player = System.GetEntity(playerId);
+	local player = System.GetEntity(playerId)
 
-	-- Verify the player is of type "player" - sanity check
 	if (player and player.player) then
+		local rnd
 
-		-- Give an AT15 to playerId into whatever slot is available and have the player select it
-		local weapon = ISM.GiveItem(playerId, "AT15", true);
+		ISM.GiveItem(playerId, "Flashlight")
 
-		-- Add a STANAGx30 to playerId into the stanag_mag00 slot of the AT15
-		-- Slot names can be found in the item XML files and they start at index 00 and increment up from there
-		local accessory = ISM.GiveItem(playerId, "STANAGx30", false, weapon.id, "stanag_mag00");
+		-- Hat
+		rnd = random(1, 3)
+
+		if (rnd == 1) then
+			ISM.GiveItem(playerId, "BeanieBrown")
+		elseif (rnd == 2) then
+			ISM.GiveItem(playerId, "BikeHelmetBlack")
+		else
+			ISM.GiveItem(playerId, "BandanaHat")
+		end
+
+		-- Feet
+		rnd = random(1, 3)
+
+		if (rnd == 1) then
+			ISM.GiveItem(playerId, "TennisShoes")
+		elseif (rnd == 2) then
+			ISM.GiveItem(playerId, "Sneakers")
+		else
+			ISM.GiveItem(playerId, "CanvasShoes")
+		end
+
+		-- Legs
+		local leg
+
+		rnd = random(1, 4)
+
+		if (rnd == 1) then
+			leg = ISM.GiveItem(playerId, "BlueJeans", true)
+		elseif (rnd == 2) then
+			leg = ISM.GiveItem(playerId, "BlueJeans2", true)
+		elseif (rnd == 3) then
+			leg = ISM.GiveItem(playerId, "BlueJeans2Brown", true)
+		else
+			leg = ISM.GiveItem(playerId, "BlueJeans2Green", true)
+		end
+
+		-- Torso
+		rnd = random(1, 6)
+
+		if (rnd == 1) then
+			ISM.GiveItem(playerId, "TshirtNoImageBlack")
+		elseif (rnd == 2) then
+			ISM.GiveItem(playerId, "TshirtNoImageBlue")
+		elseif (rnd == 3) then
+			ISM.GiveItem(playerId, "TshirtNoImageGreen")
+		elseif (rnd == 4) then
+			ISM.GiveItem(playerId, "TshirtNoImageGrey")
+		elseif (rnd == 5) then
+			ISM.GiveItem(playerId, "TshirtNoImagePink")
+		else
+			ISM.GiveItem(playerId, "TshirtNoImageRed")
+		end
+
+		-- Weapon
+		rnd = random(1, 3)
+
+		if (rnd == 1) then
+			ISM.GiveItem(playerId, "Axe")
+		elseif (rnd == 2) then
+			ISM.GiveItem(playerId, "BaseballBat")
+		else
+			ISM.GiveItem(playerId, "Crowbar")
+		end
+
+		-- Bag
+		local bag
+
+		rnd = random(1, 2)
+
+		if (rnd == 1) then
+			bag = ISM.GiveItem(playerId, "horshoe_pack", true)
+		else
+			bag = ISM.GiveItem(playerId, "CraftedFannyPack", true)
+		end
+
+		-- Food
+		rnd = random(1, 2)
+
+		if (rnd == 1) then
+			ISM.GiveItem(playerId, "Berries", false, bag.id)
+		else
+			ISM.GiveItem(playerId, "AppleFresh", false, bag.id)
+		end
+
+		-- Other items
+		ISM.GiveItem(playerId, "Bandage", false, leg.id)
+		ISM.GiveItem(playerId, "WaterBottle", false, bag.id)
 	end
 end
---]]
