328dfd4862fbf10c9a77b4812c4f705f5b0d4f31
Marc S. Brooks <marc@mbrooks.info>
2021-12-02 22:03:36 -0800

    Removed duplicate entries

--- GameSDK/Scripts/Spawners/rrai_mods/MoreHumanSpiders.lua
+++ GameSDK/Scripts/Spawners/rrai_mods/MoreHumanSpiders.lua
@@ -27,18 +27,3 @@ mutant9.spawnCategory = "HumanSpiderSingle"
 
 local mutant10 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_Spiker")
 mutant10.spawnCategory = "HumanSpiderSingle"
-
-local mutant11 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_HumanZombie")
-mutant11.spawnCategory = "HumanSpiderSingle"
-
-local mutant12 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_TwoHeadDog")
-mutant12.spawnCategory = "HumanSpiderSingle"
-
-local mutant13 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_Brute")
-mutant13.spawnCategory = "HumanSpiderSingle"
-
-local mutant14 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_Crazy")
-mutant14.spawnCategory = "HumanSpiderSingle"
-
-local mutant15 = FindInTable(AIRoundRobinManager.rraiSpawnGroups.islands, "name", "RRAIZone_Spiker")
-mutant15.spawnCategory = "HumanSpiderSingle"
