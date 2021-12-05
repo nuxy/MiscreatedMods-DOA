2b663befa99ec3ea18e791e5037197adc6812b43
Marc S. Brooks <marc@mbrooks.info>
2021-12-04 17:28:06 -0800

    Removed game-defined mutant instances
    Split logic to separate files

--- GameSDK/Scripts/Spawners/aism_mods/MoreHumanSpiders.lua
+++ GameSDK/Scripts/Spawners/aism_mods/MoreHumanSpiders.lua
@@ -10,45 +10,56 @@ mutant2.classes =
 	{ category = "HumanSpiderSingle", percent = 100 },
 }
 
-local mutant3 = FindInTable(AISpawnerManager.aiCategories, "category", "spiker_pack")
-mutant3.min = 4
-mutant3.max = 5
-
-local mutant4 = FindInTable(AISpawnerManager.aiCategories, "category", "brute_pack")
-mutant4.min = 4
-mutant4.max = 6
-
-local mutant5 = FindInTable(AISpawnerManager.aiCategories, "category", "brute_hunting_pack")
-mutant5.min = 4
-mutant5.max = 7
-
-local mutant6 = FindInTable(AISpawnerManager.aiCategories, "category", "horde")
-mutant6.classes =
+local mutant3 = FindInTable(AISpawnerManager.aiCategories, "category", "brute_armored")
+mutant3.classes =
 {
 	{ category = "HumanSpiderSingle", percent = 100 },
 }
 
-local mutant7 = FindInTable(AISpawnerManager.aiCategories, "category", "twoheaddog")
-mutant7.classes =
-{   
+local mutant4 = FindInTable(AISpawnerManager.aiCategories, "category", "twoheaddog")
+mutant4.classes =
+{
 	{ category = "HumanSpiderSingle", percent = 100 },
 }
 
-local mutant8 = FindInTable(AISpawnerManager.aiCategories, "category", "mutant_invasion")
+local mutant5 = FindInTable(AISpawnerManager.aiCategories, "category", "spiker_pack")
+mutant5.classes =
+{
+	{ category = "HumanSpiderSingle", percent = 100 },
+}
+mutant5.min = 4
+mutant5.max = 5
+
+local mutant6 = FindInTable(AISpawnerManager.aiCategories, "category", "brute_pack")
+mutant6.classes =
+{
+	{ category = "HumanSpiderSingle", percent = 100 },
+}
+mutant6.min = 4
+mutant6.max = 6
+
+local mutant7 = FindInTable(AISpawnerManager.aiCategories, "category", "brute_hunting_pack")
+mutant7.classes =
+{
+	{ category = "HumanSpiderSingle", percent = 100 },
+}
+mutant7.min = 4
+mutant7.max = 7
+
+local mutant8 = FindInTable(AISpawnerManager.aiCategories, "category", "horde")
 mutant8.classes =
 {
 	{ category = "HumanSpiderSingle", percent = 100 },
 }
 
-local mutant9 = FindInTable(AISpawnerManager.aiCategories, "category", "twoheaddog_invasion")
+local mutant9 = FindInTable(AISpawnerManager.aiCategories, "category", "mutant_invasion")
 mutant9.classes =
 {
 	{ category = "HumanSpiderSingle", percent = 100 },
 }
 
-local mutant10 = FindInTable(AISpawnerManager.aiCategories, "category", "mutant_invasion")
+local mutant10 = FindInTable(AISpawnerManager.aiCategories, "category", "twoheaddog_invasion")
 mutant10.classes =
 {
 	{ category = "HumanSpiderSingle", percent = 100 },
 }
-
---
+++ GameSDK/Scripts/Spawners/aism_mods/RemoveMutants.lua
@@ -0,0 +1,19 @@
+local mutant1 = FindInTable(AISpawnerManager.aiCategories, "category", "BruteMutantArmored")
+
+RemoveFromTable(AISpawnerManager.aiCategories, mutant1)
+
+local mutant2 = FindInTable(AISpawnerManager.aiCategories, "category", "BruteMutantSingle")
+
+RemoveFromTable(AISpawnerManager.aiCategories, mutant2)
+
+local mutant3 = FindInTable(AISpawnerManager.aiCategories, "category", "HumanZombieSingle")
+
+RemoveFromTable(AISpawnerManager.aiCategories, mutant3)
+
+local mutant4 = FindInTable(AISpawnerManager.aiCategories, "category", "SpikerSingle")
+
+RemoveFromTable(AISpawnerManager.aiCategories, mutant4)
+
+local mutant5 = FindInTable(AISpawnerManager.aiCategories, "category", "CrazySingle")
+
+RemoveFromTable(AISpawnerManager.aiCategories, mutant5)
