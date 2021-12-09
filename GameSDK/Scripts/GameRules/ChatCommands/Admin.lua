bb7feced22b13290617dfa6c472a5eea9a568a18
Marc S. Brooks <marc@mbrooks.info>
2021-12-08 16:53:46 -0800

    Added !safezone Admin chat command

--- GameSDK/Scripts/GameRules/ChatCommands/Admin.lua
+++ GameSDK/Scripts/GameRules/ChatCommands/Admin.lua
@@ -19,3 +19,18 @@ ChatCommands["!attn"] = function(playerId, message)
 		g_gameRules.game:SendTextMessage(0, 0, message)
 	end
 end
+
+-- Teleport to the Safe Zone.
+ChatCommands["!safezone"] = function(playerId)
+	Log(">> !safezone")
+
+	local player = System.GetEntity(playerId)
+
+	if player and IsAdminPlayer(player.player:GetSteam64Id()) then
+		g_Vectors.temp_v1.x = 4912.13916015625
+		g_Vectors.temp_v1.y = 4717.68994140625
+		g_Vectors.temp_v1.z = 142.735000610352
+
+		player.player:TeleportTo(g_Vectors.temp_v1)
+	end
+end
