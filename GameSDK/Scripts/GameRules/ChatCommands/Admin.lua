a495849c9c8d5b6186818877e67afbd118b69869
Marc S. Brooks <marc@mbrooks.info>
2021-12-09 15:56:41 -0800

    Refactor, added CLI arg check (use/message)
    Added missing entity call

--- GameSDK/Scripts/GameRules/ChatCommands/Admin.lua
+++ GameSDK/Scripts/GameRules/ChatCommands/Admin.lua
@@ -1,22 +1,36 @@
-Script.ReloadScript("scripts/gamerules/AccessControl.lua")
+Script.LoadScript("scripts/gamerules/AccessControl.lua")
 
 -- Execute an RCON command on the server.
 ChatCommands["!rcon"] = function(playerId, command)
 	Log(">> !rcon %s", command)
 
-	if player and IsAdminPlayer(player.player:GetSteam64Id()) then
-		System.ExecuteCommand(command)
+	local message = "Invalid command. Usage:\n  !rcon <command>"
+
+	local player = System.GetEntity(playerId)
+
+	if IsAdminPlayer(player.player:GetSteam64Id()) then
+		if not EmptyString(command) then
+			System.ExecuteCommand(command)
+		else
+			g_gameRules.game:SendTextMessage(4, playerId, message)
+		end
 	end
 end
 
 -- Send a screen message to all players.
-ChatCommands["!attn"] = function(playerId, message)
-	Log(">> !attn %s", message)
+ChatCommands["!attn"] = function(playerId, textMessage)
+	Log(">> !attn %s", textMessage)
+
+	local message = "Invalid command. Usage:\n  !attn <message>"
 
 	local player = System.GetEntity(playerId)
 
-	if player and IsAdminPlayer(player.player:GetSteam64Id()) and message then
-		g_gameRules.game:SendTextMessage(0, 0, message)
+	if IsAdminPlayer(player.player:GetSteam64Id()) then
+		if not EmptyString(textMessage) then
+			g_gameRules.game:SendTextMessage(0, 0, textMessage)
+		else
+			g_gameRules.game:SendTextMessage(4, playerId, message)
+		end
 	end
 end
 
@@ -26,7 +40,7 @@ ChatCommands["!safezone"] = function(playerId)
 
 	local player = System.GetEntity(playerId)
 
-	if player and IsAdminPlayer(player.player:GetSteam64Id()) then
+	if IsAdminPlayer(player.player:GetSteam64Id()) then
 		g_Vectors.temp_v1.x = 4912.13916015625
 		g_Vectors.temp_v1.y = 4717.68994140625
 		g_Vectors.temp_v1.z = 142.735000610352
