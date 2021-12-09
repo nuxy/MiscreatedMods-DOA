dc1ced3575e97922f66423fe13b48c8656d7f520
Marc S. Brooks <marc@mbrooks.info>
2021-12-08 23:20:08 -0800

    Increased RandomCraftedWeapons spawn propability

---
+++ GameSDK/Scripts/Spawners/ism_mods/MoreCraftedWeapons.lua
@@ -0,0 +1,14 @@
+local item = FindInTable(ItemSpawnerManager.itemCategories, "category", "RandomCraftedWeapons")
+item.classes =
+{
+	{ category = "CraftedBowWithMagazines", percent = 37 },
+	{ class = "CraftedPistol", percent = 37 },
+	{ class = "CraftedRifleLong", percent = 30 },
+	{ class = "CraftedSMG", percent = 35 },
+	{ class = "CraftedLongPistol", percent = 52 },
+	{ class = "CraftedPistol556", percent = 29 },
+	{ class = "CraftedRifle9mm", percent = 55 },
+	{ class = "CraftedShortRifle556", percent = 25 },
+	{ class = "CraftedShortShotgun", percent = 50 },
+	{ class = "CraftedShotgun", percent = 30 },
+}
