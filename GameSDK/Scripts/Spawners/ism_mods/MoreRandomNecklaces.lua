1fcbf374aa4f1427b90beb11ee092a0cb31ca12e
Marc S. Brooks <marc@mbrooks.info>
2021-12-10 23:39:04 -0800

    Increased necklace spawning probability

---
+++ GameSDK/Scripts/Spawners/ism_mods/MoreRandomNecklaces.lua
@@ -0,0 +1,10 @@
+local item = FindInTable(ItemSpawnerManager.itemCategories, "category", "RandomNeck")
+item.classes =
+{
+	{ category = "RandomScarf", percent = 45 },
+	{ class = "NecklaceCrossChrome", percent = 30 },
+	{ class = "ShoulderPadOneSide", percent = 10 },
+	{ class = "ShoulderPadOneSideLeft", percent = 10 },
+	{ class = "NecklaceCrossGold", percent = 20 },
+	{ class = "NecklaceCrossSilver", percent = 25 },
+}
