d6bda4c3620ee88b32bb3d46e2e882a4741c75bc
Marc S. Brooks <marc@mbrooks.info>
2021-12-16 21:13:31 -0800

    Added admin protected chat commands

--- GameSDK/Scripts/GameRules/ChatCommands/Player.lua
+++ GameSDK/Scripts/GameRules/ChatCommands/Player.lua
@@ -1,14 +1,28 @@
+Script.LoadScript("scripts/gamerules/AccessControl.lua")
+
 -- Output to console help menu.
 ChatCommands["!help"] = function(playerId)
 	Log(">> !help")
+
+	local player = System.GetEntity(playerId)
 
 	local message = [[
 Supported Commands
   !stats <pve|pvp>
   !kills <pve|pvp>
   !top10 <pve|pvp>
-  !report <player>
+  !report <PLAYER>
 ]]
+
+	if IsAdminPlayer(player.player:GetSteam64Id()) then
+		message = message .. [[
+  !rcon <command>
+  !wall <message>
+  !ban <64BITSTEAMID>
+  !kick <64BITSTEAMID>
+  !safezone
+]]
+	end
 
 	g_gameRules.game:SendTextMessage(4, playerId, message)
 end
