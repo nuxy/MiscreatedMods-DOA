4e9f7d7470bef527625e03266fac7bfd2aadaed1
Marc S. Brooks <marc@mbrooks.info>
2022-03-29 10:08:10 -0700

    Updated incorrect actionables value

--- GameSDK/Scripts/Spawners/asm_mods/BalancedFoodDrink.lua
+++ GameSDK/Scripts/Spawners/asm_mods/BalancedFoodDrink.lua
@@ -1,10 +1,11 @@
 local action1 = FindInTable(ActionableWorldManager.actionables, "text", "@drink_from_pump")
 action1.drinkAmount = 600
+action1.regenerate = 30
 
 local action2 = FindInTable(ActionableWorldManager.actionables, "text", "@drink_from_hydrant")
 action2.drinkAmount = 400
 action2.regenerate = 30
 
-local action3 = FindInTable(ActionableWorldManager.actionables, "text", "RandomDrinkVendingMachineContent")
+local action3 = FindInTable(ActionableWorldManager.actionables, "text", "@search_vending_machine")
 action3.regenerate = 60
 action3.percentage = 80
