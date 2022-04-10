b6c085ae614944358ebebd1a8f02bcc4a53a83c1
Marc S. Brooks <marc@mbrooks.info>
2022-04-10 15:48:03 -0700

    Updated to spawn at PlotSign, if available

--- GameSDK/Scripts/mods/RandomSpawnPoint.lua
+++ GameSDK/Scripts/mods/RandomSpawnPoint.lua
@@ -13,22 +13,37 @@ function Miscreated:InitPlayer(playerId)
 	local player = System.GetEntity(playerId)
 
 	if player and player.player then
-		if not IsEmptyCollection(DB, "LocationCollection") then
+		local pos = {x=0, y=0, z=0}
+		local rot = {x=0, y=0, z=0}
 
-			-- Get random location.
+		-- Spawn at base PlotSign position.
+		for _, plotsign in ipairs(BaseBuildingSystem:GetPlotSigns()) do
+			local steamId = player.player:GetSteam64Id()
+
+			if plotsign.plotsign:GetOwnerSteam64Id() == steamId then
+				pos = plotsign:GetWorldPos()
+				rot = plotsign:GetAngles()
+				break
+			end
+		end
+
+		-- Spawn at Admin defined location.
+		if IsNullVector(pos) and not IsEmptyCollection(DB, "LocationCollection") then
 			local data = GetRandomPage(DBCollection)
 
 			if data ~= nil then
-				g_Vectors.temp_v1.x = tonumber(data["posX"])
-				g_Vectors.temp_v1.y = tonumber(data["posY"])
-				g_Vectors.temp_v1.z = tonumber(data["posZ"])
+				pos.x = tonumber(data["posX"])
+				pos.y = tonumber(data["posY"])
+				pos.z = tonumber(data["posZ"])
 
-				local angle = player:GetAngles()
-				angle.z = tonumber(data["posR"])
-
-				player:SetAngles(angle)
-				player:SetWorldPos(g_Vectors.temp_v1)
+				rot = player:GetAngles()
+				rot.z = tonumber(data["posR"])
 			end
+		end
+
+		if IsNotNullVector(pos) and IsNotNullVector(rot) then
+			player:SetAngles(rot)
+			player:SetWorldPos(pos)
 		end
 	end
 end
