0c8866fb1a553d39b089141465191abd0931e82e
Marc S. Brooks <marc@mbrooks.info>
2022-06-03 11:42:03 -0700

    Replaced monkey-patch with callback

--- GameSDK/Scripts/mods/GlobalPlayerCallout.lua
+++ GameSDK/Scripts/mods/GlobalPlayerCallout.lua
@@ -1,12 +1,13 @@
 --
 -- Globally callout connecting players.
 --
-RegisterCallback(Miscreated.Server, "OnClientEnteredGame", nil,
-	function(channelId, player, loadingSaveGame)
+RegisterCallbackReturnAware(Miscreated.Server, "OnClientEnteredGame", nil,
+	function(self, channelId, player, loadingSaveGame)
 		--Log(">> Miscreated.Server:OnClientEnteredGame")
 
 		local message = player:GetName() .. " has entered the game"
 
 		g_gameRules.game:SendTextMessage(0, 0, message)
+		return ret
 	end
 )
