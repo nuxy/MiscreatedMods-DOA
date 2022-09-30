9f8d4c7a3a1354e49c0244526d333f70e639c465
Marc S. Brooks <marc@mbrooks.info>
2022-09-30 13:17:59 -0700

    Revert Callback since it doesn't work

--- GameSDK/Scripts/mods/GlobalPlayerCallout.lua
+++ GameSDK/Scripts/mods/GlobalPlayerCallout.lua
@@ -1,13 +1,10 @@
 --
 -- Globally callout connecting players.
 --
-RegisterCallbackReturnAware(Miscreated.Server, "OnClientEnteredGame", nil,
-	function(self, channelId, player, loadingSaveGame)
-		--Log(">> Miscreated.Server:OnClientEnteredGame")
+function Miscreated.Server:OnClientEnteredGame(channelId, player, loadingSaveGame)
+	--Log(">> Miscreated.Server:OnClientEnteredGame")
 
-		local message = player:GetName() .. " has entered the game"
+	local message = player:GetName() .. " has entered the game"
 
-		g_gameRules.game:SendTextMessage(0, 0, message)
-		return ret
-	end
-)
+	g_gameRules.game:SendTextMessage(0, 0, message)
+end
